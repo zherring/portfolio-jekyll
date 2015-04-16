fb083a24e37a4b2faf69a27e9efc145b56c82683
zherring <z.herring@gmail.com>
2015-04-15 23:33:14 -0600

    More body work

--- js/main.js
+++ js/main.js
@@ -9,27 +9,26 @@ $('li').each(function(){
 var menuPlacement = function(e) {
     var liIndex = $('li.active').index();
     var placement = 0;
-    for (var i = 0; i <= liIndex; i++) {
-        if (i < liIndex) {
-            placement += menuArray[i];
-            window.console.log("fired!");
+        for (var i = 0; i <= liIndex; i++) {
+            if (i < liIndex) {
+                placement += menuArray[i];
 
-        } else if (i === liIndex) {
-            placement += Math.round(menuArray[i]/2);
-            break;
-        } else { break; }
-    }
-    window.console.log(placement, "placement", e);
-    $('nav ul').css('margin-left', -placement);
+            } else if (i === liIndex) {
+                placement += Math.round(menuArray[i]/2);
+                break;
+            } else { break; }
+        }
+        $('nav ul').css('margin-left', -placement);
 };
 
 menuPlacement();
 
 var activePane = function(e) {
-    $('#content-container > div').removeClass('active');
-    $('#content-container > div:nth-child('+e+1+')').addClass('active');
+    // correct the index so the child-of matches
+    var childOf = e + 1;
+    $('.panel').removeClass('active');
+    $('#content-container > div:nth-child('+childOf+')').addClass('active');
 };
-
 
 
 $('nav li').click(function(){
@@ -39,19 +38,3 @@ $('nav li').click(function(){
     menuPlacement(varClick);
     activePane(varClick);
 });
-//window.console.log(placement);
-// if (var index = 0; index < liIndex; index++) {
-//     window.console.log("fired");
-// }
-//
-// if (var i = 0; i <= liIndex; i++) {
-//      placement += menuArray[i];
-//     // window.console.log(menuArray[i]);
-//     //   window.console.log("firing!");
-// }
-//
-//
-// window.console.log(placement);
-// for (i = 0; i < 5; i++) {
-//     text += "The number is " + i + "<br>";
-// }
